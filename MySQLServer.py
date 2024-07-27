b9bb325d242743520e171c747fde65f3f729c52a
Solomon Marita <officialmokua@gmail.com>
2024-07-27 17:59:25 +0300

    Added specific exception to handle

--- MySQLServer.py
+++ MySQLServer.py
@@ -15,7 +15,7 @@ mycursor = mydb.cursor()
 try:
     mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store;")
     print("Database 'alx_book_store' created successfully!")
-except:
+except mysql.connector.Error:
     print("Failed to connect to DB!")
 
 # Close connection to the databasse 
