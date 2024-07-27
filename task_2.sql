4bb5d1cb0878e01bd5cd778a6e04c8d019f2a4f3
Solomon Marita <officialmokua@gmail.com>
2024-07-27 18:01:36 +0300

    Added schema for specified table

---
+++ task_2.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+
+CREATE TABLE Books(
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130),
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id),
+    price DOUBLE,
+    publication_date DATE
+);
+
+CREATE TABLE Authors (
+    author_id PRIMARY KEY,
+    author_name VARCHAR(215)
+);
+
+CREATE Table Customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+);
+
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+    order_date DATE
+);
+
+CREATE TABLE Order_Details (
+    order_detail_id PRIMARY KEY,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
+    quantity DOUBLE
+);
